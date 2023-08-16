e3cc29ed03e68f13227bce4acf71643780572f1d
Adian Engler <aidanengler@gmail.com>
2023-08-16 05:36:31 -0500

    update

---
+++ footer.php
@@ -0,0 +1,65 @@
+<footer class="site-footer">
+      <div class="site-footer__inner container container--narrow">
+        <div class="group">
+          <div class="site-footer__col-one">
+            <h1 class="school-logo-text school-logo-text--alt-color">
+              <a href="<?php echo site_url() ?>"><strong>Dean's Wrecker</strong></a>
+            </h1>
+            <p><a class="site-footer__link" href="#"></a></p>
+          </div>
+
+          <div class="site-footer__col-two-three-group">
+            <div class="site-footer__col-two">
+              <h3 class="headline headline--small">Services</h3>
+              <nav class="nav-list">
+                <ul>
+                  <li><a href="<?php echo site_url("about-us") ?>">About Us</a></li>
+                  <li><a href="<?php echo site_url("Auto-Towing") ?>">Auto Towing</a></li>
+                  <li><a href="<?php echo site_url("Heavy-Duty-Towing") ?>">Heavy Duty Towing</a></li>
+                  <li><a href="<?php echo site_url("Crane-Service") ?>">Crane Service</a></li>
+                  <li><a href="<?php echo site_url("Equipment-Relocation") ?>">Equipment Relocation</a></li>
+                </ul>
+              </nav>
+            </div>
+
+            <div class="site-footer__col-three">
+              <h3 class="headline headline--small">Contact</h3>
+              <nav class="nav-list">
+                <ul>
+                  <li><a href="#">919-872-4476</a></li>
+                  <li><a href="#">888-269-3211</a></li>
+                  <li><a href="#">deanswreckerservice@gmail.com</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+
+          <div class="site-footer__col-four">
+            <h3 class="headline headline--small">Connect With Us</h3>
+            <nav>
+              <ul class="min-list social-icons-list group">
+                <li>
+                  <a href="#" class="social-color-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                </li>
+                <li>
+                  <a href="#" class="social-color-twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                </li>
+                <li>
+                  <a href="#" class="social-color-youtube"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+                </li>
+                <li>
+                  <a href="#" class="social-color-linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                </li>
+                <li>
+                  <a href="#" class="social-color-instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,111 @@
+<?php  get_header();?>
+<div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri("/images/Deans1.jpeg")?>)"></div>
+      <div class="page-banner__content container t-center c-white">
+          <h1 class="headline headline--large">Dean's</h1>
+        <h2 class="headline headline--medium">Family Owned & Operated
+Since 1985!</h2>
+        <h3 class="headline headline--small">Quality Service Is
+Our #1 Priority.</h3>
+        <a href="#" class="btn btn--large btn--blue">Need Help?</a>
+      </div>
+    </div>
+
+    <div class="full-width-split group">
+      <div class="full-width-split__one">
+        <div class="full-width-split__inner">
+          <h2 class="headline headline--small-plus t-center">Contact Info</h2>
+
+          <div class="event-summary">
+            <a class="event-summary__date t-center" href="#">
+              <span class="event-summary__month">Mar</span>
+              <span class="event-summary__day">25</span>
+            </a>
+            <div class="event-summary__content">
+              <h5 class="event-summary__title headline headline--tiny"><a href="#">Local Phone</a></h5>
+              <p>919-872-4476<a href="#" class="nu gray">Learn more</a></p>
+            </div>
+          </div>
+          <div class="event-summary">
+            <a class="event-summary__date t-center" href="#">
+              <span class="event-summary__month">Apr</span>
+              <span class="event-summary__day">02</span>
+            </a>
+            <div class="event-summary__content">
+              <h5 class="event-summary__title headline headline--tiny"><a href="#">Toll Fee</a></h5>
+              <p><a href="#" class="nu gray">Learn more</a></p>
+            </div>
+          </div>
+          <div class="event-summary">
+            <a class="event-summary__date t-center" href="#">
+              <span class="event-summary__month">Apr</span>
+              <span class="event-summary__day">1</span>
+            </a>
+            <div class="event-summary__content">
+              <h5 class="event-summary__title headline headline--tiny"><a href="#">Email</a></h5>
+              <p>deanswreckerservice@gmail.com <a href="#" class="nu gray">Learn more</a></p>
+            </div>
+          </div>
+
+          <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
+        </div>
+      </div>
+      <div class="full-width-split__two">
+        <div class="full-width-split__inner">
+          <h2 class="headline headline--small-plus t-center">Dean's Wrecker Service, Inc</h2>
+
+          <div class="event-summary">
+
+            <div class="event-summary__content">
+              <h5 class="event-summary__title headline headline--tiny"><a href="#"></a></h5>
+              <p><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d51730.99720309891!2d-78.585584!3d35.868926!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89ac5994d6749243%3A0x89e59eb7d0fc1cc0!2sDean&#39;s%20Wrecker%20Service%2C%20Inc!5e0!3m2!1sen!2sus!4v1692172059968!5m2!1sen!2sus" width="400" height="271" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><a href="#"</p>
+            </div>
+          </div>
+
+          <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="hero-slider">
+      <div data-glide-el="track" class="glide__track">
+        <div class="glide__slides">
+          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri("images/Crane-Service-Raleigh-copy.jpg")?>)">
+            <div class="hero-slider__interior container">
+              <div class="hero-slider__overlay">
+                <h2 class="headline headline--medium t-center">Crane Service</h2>
+                <p class="t-center">Quality Crane Services & Machine Rigging.</p>
+                <p class="t-center no-margin"><a href="<?php echo site_url("Crane-Service")?>" class="btn btn--blue">Learn more</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri("images/towingSlide.jpeg")?>)">
+            <div class="hero-slider__interior container">
+              <div class="hero-slider__overlay">
+                <h2 class="headline headline--medium t-center">Auto Towing</h2>
+                <p class="t-center">Professional Towing Services at an Affordable Price.</p>
+                <p class="t-center no-margin"><a href="<?php echo site_url("Auto-Towing")?>" class="btn btn--blue">Learn more</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri("images/Raleigh-Heavy-Duty-Towing-copy.jpg")?>)">
+            <div class="hero-slider__interior container">
+              <div class="hero-slider__overlay">
+                <h2 class="headline headline--medium t-center">Heavy Duty Towing</h2>
+                <p class="t-center">Heavy Duty Trucks for Heavy Duty Hauling Services.</p>
+                <p class="t-center no-margin"><a href="<?php echo site_url("Heavy-Duty-Towing")?>" class="btn btn--blue">Learn more</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="slider__bullets glide__bullets" data-glide-el="controls[nav]"></div>
+      </div>
+    </div>
+    <div>
+
+<?php
+get_footer();
+?>
+
+
+
